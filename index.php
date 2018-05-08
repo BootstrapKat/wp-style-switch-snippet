991b25314c7cf1e83adfc192bacf05e14e4cae94
Jason Wilson <jason@wilsons.io>
2018-05-08 16:45:48 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/style.css" />
+
+        <?php $colorScheme = get_query_var( 'color_scheme', 'blue' ) ?>
+
+        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/style-partials/<?php echo $colorScheme;?>.css" />
+
+    </head>
+    <body>
+        <div class="color-change-bar"><p>Wow, you can change my color scheme! <a href="?color_scheme=red">red</a> <a href="?color_scheme=green">green</a> <a href="?color_scheme=blue">blue</a></p></div>
+        <main class="wrapper">
+            <div class="box">
+                <h1 class="box-title">Yo.</h1>
+            </div>    
+        </main>
+    </body>
+</html>
