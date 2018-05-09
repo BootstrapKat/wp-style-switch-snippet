ba46fd2b29b9be4069eefb996faa947036921f49
Jason Wilson <jason@wilsons.io>
2018-05-09 08:37:05 -0400

    Page transition resolution

--- functions.php
+++ functions.php
@@ -1,11 +1,5 @@
 <?php
 
-    function add_query_vars_filter( $vars ){
-        $vars[] = "color_scheme";
-        return $vars;
-    }
-    add_filter( 'query_vars', 'add_query_vars_filter' );
-
     function switcher_add_custom_controls($wp_customize) {
 
         $wp_customize->add_section( 'color_scheme_section', array(
--- index.php
+++ index.php
@@ -2,17 +2,41 @@
     <head>
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/style.css" />
 
-        <?php $colorScheme = get_query_var( 'color_scheme', get_theme_mod('color_scheme_radio_setting') ) ?>
+        <?php 
+            // Check if the 'color_scheme' parameter exists in the request
+            if( isset($_GET['color_scheme'])) {
+                // If it is, set the color scheme from the parameter
+                $colorScheme = $_GET['color_scheme'];
+            } else {
+                // Otherwise use the value from the customizer
+                $colorScheme = get_theme_mod('color_scheme_radio_setting');
+            }
+            //$colorScheme = get_query_var( 'color_scheme', get_theme_mod('color_scheme_radio_setting') ); 
+            // If we use get_query_var() Wordpress tries to process the new url and winds up leaving the static front page.
+            
+        ?>
 
-        <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>/style-partials/<?php echo $colorScheme;?>.css" />
+        <link 
+            rel="stylesheet" 
+            type="text/css" 
+            href="<?php bloginfo('stylesheet_directory'); ?>/style-partials/<?php echo $colorScheme;?>.css" />
 
     </head>
     <body>
-        <div class="color-change-bar"><p>Wow, you can change my color scheme! <a href="?color_scheme=red">red</a> <a href="?color_scheme=green">green</a> <a href="?color_scheme=blue">blue</a></p></div>
+        <div class="color-change-bar">
+            <p>Wow, you can change my color scheme! 
+                <a href="?color_scheme=red">red</a>
+                <a href="?color_scheme=green">green</a>
+                <a href="?color_scheme=blue">blue</a>
+            </p>
+        </div>
         <main class="wrapper">
             <div class="box">
                 <h1 class="box-title">Yo.</h1>
             </div>    
         </main>
+        <div>
+            <?php do_action( 'cws_content' ); ?>
+        </div>
     </body>
 </html>
